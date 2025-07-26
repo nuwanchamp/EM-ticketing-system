7b573cf8721f6bdcb4905f82f2b1f26080a993bd
nuone <nuwanchamp02@gmail.com>
2025-07-26 09:46:14 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+resources used
+http://svgrepo.com/svg/528744/ticket
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=poppins:200,400,600" rel="stylesheet" />
+
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="dark dark:bg-zinc-800 text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+<header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+    @if (Route::has('login'))
+        <nav class="flex items-center justify-end gap-4">
+            @auth
+                <a
+                    href="{{ url('/dashboard') }}"
+                    class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                >
+                    Dashboard
+                </a>
+            @else
+                <a
+                    href="{{ route('login') }}"
+                    class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                >
+                    Log in
+                </a>
+
+                @if (Route::has('register'))
+                    <a
+                        href="{{ route('register') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                        Register
+                    </a>
+                @endif
+            @endauth
+        </nav>
+    @endif
+</header>
+<div
+    class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+    <main class="flex max-w-[335px] w-full  lg:max-w-4xl flex-col justify-center">
+        <span class="flex justify-center h-36">
+                <x-app-logo-icon class="size-9"/>
+        </span>
+        <h1 class=" text-xl text-primary text-center "> {{'Online Ticketing System'}}</h1>
+        <p class="text-gray-400 text-center"> Lorem ipsum site amente</p>
+        <h2 class=" text-3xl text-white text-center mt-10 "> {{\Illuminate\Support\Str::upper('Welcome')}}</h2>
+    </main>
+</div>
+
+@if (Route::has('login'))
+    <div class="h-14.5 hidden lg:block"></div>
+@endif
+</body>
+</html>
